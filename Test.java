0d628d24a0bc56e7c5f0dff75a7181685c3010b1
air75 <laksariy@yahoo.fr>
2019-09-15 02:28:01 +0200

    creation de la class principale du programme

---
+++ Test.java
@@ -0,0 +1,17 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TEST {
+
+
+	public static void main(String[] args) throws Exception  {
+
+       
+
+
+	}  
+
+
+}
